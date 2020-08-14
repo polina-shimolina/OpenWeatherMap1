717ef038df27cd599814d4bd90cc07117a40bbc6
polina-shimolina <shimolina.polina@gmail.com>
2020-08-14 14:42:20 +0300

    s

---
+++ WindowsFormsApp3/WindowsFormsApp3/Class.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    class Class
+    {
+        
+        public string result;
+        WebRequest request;
+        Weather weather;
+        public Class(string u)
+        {
+            request = WebRequest.Create(u);
+        }
+        public void Search()
+        { 
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            MessageBox.Show(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            weather = JsonConvert.DeserializeObject<Weather>(responseFromServer);
+            result = $" t in {weather.name}: {weather.main.temp} C";
+            MessageBox.Show(result);
+        }
+    }
+}
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public string url;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    {
+
+                        url = "http://api.openweathermap.org/data/2.5/weather?q=Moscow&appid=ecc0ea77210f8032ef8804a80a9f40f0";
+                        break;
+                    }
+                case 1:
+                    {
+                        url = "http://api.openweathermap.org/data/2.5/weather?q=London&appid=ecc0ea77210f8032ef8804a80a9f40f0";
+                        break;
+                    }
+                case 2:
+                    {
+                        url = "http://api.openweathermap.org/data/2.5/weather?q=Washington&appid=ecc0ea77210f8032ef8804a80a9f40f0";
+                        break;
+                    }
+                case 3:
+                    {
+                        url = "http://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=ecc0ea77210f8032ef8804a80a9f40f0";
+                        break;
+                    }
+                case 4:
+                    {
+                        url = "http://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=ecc0ea77210f8032ef8804a80a9f40f0";
+                        break;
+                    }
+            }
+            Class weath = new Class(url);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
